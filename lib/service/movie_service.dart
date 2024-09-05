e5c988881d34644ebd8b6d1408f2441446c8c106
muhammadovierakasiwi <muhammad.siwi@indocyber.id>
2024-09-05 16:28:11 +0700

    Changes service

--- lib/service/movie_service.dart
+++ lib/service/movie_service.dart
@@ -2,22 +2,14 @@ import 'package:movie_module/model/model_path.dart';
 import 'package:shared_module/shared.dart';
 
 class MovieService {
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: dotenv.env['BASE_URL']!,
-      connectTimeout: const Duration(seconds: 30),
-      receiveTimeout: const Duration(seconds: 30),
-      sendTimeout: const Duration(seconds: 30),
-      validateStatus: (_) => true,
-      queryParameters: {
-        'api_key': dotenv.env['API_KEY'],
-      },
-    ),
-  );
+  final Dio _dio = Dio();
 
   Future<MovieResponse> fetchMovie() async {
     final response = await _dio.get(
-      UrlList.discoverMovie,
+      '${dotenv.env['BASE_URL']}${UrlList.discoverMovie}',
+      queryParameters: {
+        'api_key': dotenv.env['API_KEY'],
+      },
     );
 
     debugPrint('${response.data}');
